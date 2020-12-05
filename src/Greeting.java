f6cf9ed5e6d41a068375d9cd86ebf9c9ac4ea26c
varsha c <varshacemail@gmail.com>
2020-12-05 15:22:25 +0530

    test

---
+++ src/Greeting.java
@@ -0,0 +1,16 @@
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println(args[0]+args[1]);
+        if(args[0].equals("Student"))
+        {
+
+            System.out.println("Hello Student");
+        }
+        else
+        {
+
+            System.out.println("Hello World");
+        }
+
+    }
+}
